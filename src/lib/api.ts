774db8a6ec3045b3ec551757e249cabc71a1b643
igorfloresdev <dev.igorflores@gmail.com>
2023-09-01 18:59:45 -0300

    Added headers axios instance

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
 })
 
 export default API
